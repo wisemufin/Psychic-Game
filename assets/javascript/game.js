62451759f1e03c835df051998ac1686ccb067949
Matthew Trabold <mct4240@gmail.com>
2019-03-03 17:04:40 -0600

    started making the game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,5 +4,14 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz";
 // Breaks the alphabet variable into an array of strings
 var letterChoice = alphabet.split("");
 
-// Computer randomly chooses a letter of the alphabet array
-var computerChoice = letterChoice[Math.floor(Math.random()*letterChoice.length)];
+// This function runs on keypres
+document.onkeyup = function(event) {
+
+  // Logs the key that was pressed into the userGuess variable
+  var userGuess = event.key;
+
+  // Computer randomly chooses a letter of the alphabet array
+  var computerChoice = letterChoice[Math.floor(Math.random()*letterChoice.length)];
+
+
+}
